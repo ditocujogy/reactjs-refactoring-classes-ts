2974edb1220d32a8690adabc53cfcc1eec2b89ea
Matheus C <matheuschaves21@gmail.com>
2021-03-25 12:57:52 -0300

    done

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,24 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.FC<IconProps>;
+}
+
+interface IconProps {
+  size: number;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface FoodInput {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  available: boolean;
+}
